71318b304eb8f69edc4a3d62ef8f51df89d2c25a
Rango ! <baloney7987@gmail.com>
2026-04-26 14:07:17 -0400

    Clear pageTitleSuffix in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "rango lf",
-    pageTitleSuffix: "rango lf",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
