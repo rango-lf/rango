a475bc825b97f683aa5cd3c8604ba0eca42c06d7
rango-lf <baloney7987@gmail.com>
2026-04-27 10:38:33 -0400

    Quartz sync: Apr 27, 2026, 10:38 AM

--- quartz/components/Webring.tsx
+++ quartz/components/Webring.tsx
@@ -1,22 +1,41 @@
-import { QuartzComponent, QuartzComponentProps } from "./types"
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
-const Webring: QuartzComponent = (_props: QuartzComponentProps) => {
-  return (
-    <div style={{
-      display: "flex", alignItems: "center", gap: "15px",
-      backgroundColor: "var(--light)", padding: "5px 15px",
-      borderRadius: "8px", border: "1px solid var(--lightgray)"
-    }}>
-      <a href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/?nav=prev"
-        style={{ color: "var(--dark)", textDecoration: "none", fontSize: "16px" }}>←</a>
-      <a href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/" target="_blank">
-        <img src="https://WebRing.skule.ca/img/icon.svg" alt="SKULE WebRing"
-          style={{ width: "34px", height: "24px", marginBottom: "-6px" }} />
-      </a>
-      <a href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/?nav=next"
-        style={{ color: "var(--dark)", textDecoration: "none", fontSize: "16px" }}>→</a>
-    </div>
-  )
-}
+export default (() => {
+  const Webring = (_props: QuartzComponentProps) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "15px",
+          backgroundColor: "var(--light)",
+          padding: "5px 15px",
+          borderRadius: "8px",
+          border: "1px solid var(--lightgray)",
+        }}
+      >
+        
+          href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/?nav=prev"
+          style={{ color: "var(--dark)", textDecoration: "none", fontSize: "16px" }}
+        >
+          ←
+        </a>
+        <a href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/" target="_blank">
+          <img
+            src="https://WebRing.skule.ca/img/icon.svg"
+            alt="SKULE WebRing"
+            style={{ width: "34px", height: "24px", marginBottom: "-6px" }}
+          />
+        </a>
+        
+          href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/?nav=next"
+          style={{ color: "var(--dark)", textDecoration: "none", fontSize: "16px" }}
+        >
+          →
+        </a>
+      </div>
+    )
+  }
 
-export default Webring
+  return Webring
+}) satisfies QuartzComponentConstructor
