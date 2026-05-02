85899b7be9836b67e7551d09be880ca42935fcca
rango-lf <baloney7987@gmail.com>
2026-05-02 19:22:33 -0400

    Quartz sync: May 2, 2026, 7:22 PM

--- quartz/components/Webring.tsx
+++ quartz/components/Webring.tsx
@@ -4,6 +4,7 @@ export default (() => {
   const Webring = (_props: QuartzComponentProps) => {
     return (
       <>
+      <div class={classNames(displayClass, "webrings")}>
         <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "10px", backgroundColor: "var(--light)", padding: "5px 10px", borderRadius: "8px", border: "1px solid var(--lightgray)" }}>
           <a href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/?nav=prev" style={{ color: "var(--dark)", textDecoration: "none", fontSize: "16px" }}>{"⬅️"}</a>
           <a href="https://WebRing.skule.ca/#https://rango-lf.github.io/rango-lf/" target="_blank"><img src="https://WebRing.skule.ca/img/icon.svg" alt="SKULE WebRing" style={{ width: "44px", height: "44px" }} /></a>
