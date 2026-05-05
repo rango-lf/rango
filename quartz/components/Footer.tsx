1fb456c4f835e2ab5ed365386d8a3f232519d18d
rango-lf <baloney7987@gmail.com>
2026-05-05 19:24:19 -0400

    Quartz sync: May 5, 2026, 7:24 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,6 +13,7 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        <img src="https://rainy.gay/pride/bv/progress.png" alt="Progress Pride Flag" style="height: 34px"/>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
